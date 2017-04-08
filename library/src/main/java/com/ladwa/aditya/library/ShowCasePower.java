7a37abadbb333cf57a27136064a0b84817b904b5
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-04-08 18:57:42 +0530

    fect(API): Added API contract for Builder and ShowCasePower class

--- library/src/main/java/com/ladwa/aditya/library/ShowCasePower.java
+++ library/src/main/java/com/ladwa/aditya/library/ShowCasePower.java
@@ -1,6 +1,7 @@
 package com.ladwa.aditya.library;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -8,13 +9,17 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+
+import com.ladwa.aditya.library.api.BuilderContract;
+import com.ladwa.aditya.library.api.ShowCasePowerContract;
 
 /**
  * A Custom {@link android.widget.FrameLayout} that represents Showcasepower
  * Created by Aditya on 08-Apr-17.
  */
-public class ShowCasePower extends FrameLayout {
+public class ShowCasePower extends FrameLayout implements ShowCasePowerContract {
 
     private static final String TAG = ShowCasePower.class.getSimpleName();
 
@@ -47,6 +52,42 @@ public class ShowCasePower extends FrameLayout {
      */
     private void init(Context context) {
         LayoutInflater.from(context).inflate(R.layout.content_details, this, true);
-        Log.d(TAG, "Called");
+        Log.d(TAG, "Initialized");
+    }
+
+    @Override
+    public boolean show(Builder builder) {
+        ((ViewGroup) builder.getActivity().getWindow().getDecorView()).addView(this);
+        return true;
+    }
+
+    /**
+     * Builder class that implements {@link BuilderContract}
+     * for building {@link ShowCasePower}
+     */
+    public static class Builder implements BuilderContract {
+
+        private final Activity activity;
+        private ShowCasePower showCasePower;
+
+        public Builder(Activity activity) {
+            this.activity = activity;
+            showCasePower = new ShowCasePower(activity);
+        }
+
+        private ShowCasePower build() {
+            return showCasePower;
+        }
+
+        @Override
+        public ShowCasePower show() {
+            build();
+            return showCasePower;
+        }
+
+        @Override
+        public Activity getActivity() {
+            return activity;
+        }
     }
 }
