7b051a61427969461fd923cd7bedd81d75c1cab8
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-04-08 14:56:17 +0530

    fect(ShowCasePower): Added basic initialization

--- library/src/main/java/com/ladwa/aditya/library/ShowCasePower.java
+++ library/src/main/java/com/ladwa/aditya/library/ShowCasePower.java
@@ -6,27 +6,43 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 
 /**
  * A Custom {@link android.widget.FrameLayout} that represents Showcasepower
  * Created by Aditya on 08-Apr-17.
  */
-public class ShowCasePower extends FrameLayout{
+public class ShowCasePower extends FrameLayout {
     public ShowCasePower(@NonNull Context context) {
         super(context);
+        init(context);
     }
 
     public ShowCasePower(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public ShowCasePower(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public ShowCasePower(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+
+    /**
+     * Basic initialization which is called when the view is created
+     *
+     * @param context
+     */
+    private void init(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.content_details, this, true);
+
     }
 }
